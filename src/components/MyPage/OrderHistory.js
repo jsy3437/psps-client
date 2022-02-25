a424db674a9281f81b15b11e45e6a9b674491832
Uni <joseoyun12@gmail.com>
2022-02-25 17:16:07 +0900

    psps 마이페이지 주문내역 레이아웃, 스타일링 수정

--- src/components/MyPage/OrderHistory.js
+++ src/components/MyPage/OrderHistory.js
@@ -20,43 +20,43 @@ const OrderHistory = (props) => {
 		}
 	}, [props.location.state]);
 
+	console.log(props.paymentList);
+
 	return (
 		<MyPageInside>
 			{props.paymentList.length === 0 && (
 				<OrderHistoryWrap>
 					<NothingText>현재 주문하신 내역이 없습니다.</NothingText>
 					<GoShoppingButton>쇼핑하기</GoShoppingButton>
-					<GrayBackground />
 				</OrderHistoryWrap>
 			)}
 			{props.paymentList.length > 0 && !viewDetail && (
 				<OrderHistoryWrap>
 					{props.paymentList &&
 						props.paymentList.map((el, idx) => (
-							<OrderHistoryList key={idx}>
+							<OrderHistoryList
+								key={idx}
+								onClick={() => {
+									goDetail(el.payment_id);
+								}}
+							>
+								<OrderTopDate>
+									{el.create_at.split('T')[0]}
+									<OrderId>・{el.payment_uid}</OrderId>
+								</OrderTopDate>
 								<ListImg alt="product img" src={ex1} />
 								<ListContents>
-									<OrderTop>
-										<OrderTopText date>
-											{`${el.create_at.split('T')[0]} 주문`}
-										</OrderTopText>
-										<OrderTopText state>{el.process}</OrderTopText>
-									</OrderTop>
-									<ProductName>{el.name}</ProductName>
-									{/* <ProductOption>{el.option}</ProductOption> */}
-									<ProductCount>{`${el.amount}원`}</ProductCount>
+									<ProductNameBox>
+										<ProductName>{el.name}</ProductName>
+										<ProductNameSpan>외 3건</ProductNameSpan>
+									</ProductNameBox>
+									<DeliveryAddr>
+										{`(${el.del_postcode}) ${el.del_addr.split('/')[0]} ${
+											el.del_addr.split('/')[1]
+										}`}
+									</DeliveryAddr>
+									<ProductTotalPrice>{`총 결제 금액 ${el.amount.toLocaleString()}원`}</ProductTotalPrice>
 								</ListContents>
-								<ListButtons>
-									<ListButton>배송조회</ListButton>
-									<ListButton
-										onClick={() => {
-											goDetail(el.payment_id);
-										}}
-									>
-										주문 상세보기
-									</ListButton>
-									<ListButton last>교환, 반품 신청</ListButton>
-								</ListButtons>
 							</OrderHistoryList>
 						))}
 					<PageSelector
@@ -65,7 +65,6 @@ const OrderHistory = (props) => {
 						page={page}
 						setPage={setPage}
 					/>
-					<GrayBackground />
 				</OrderHistoryWrap>
 			)}
 			{props.paymentList.length > 0 && viewDetail && (
@@ -83,6 +82,7 @@ const MyPageInside = styled.div`
 	align-items: center;
 	position: relative;
 	margin-bottom: 7.7rem;
+	margin-top: 2rem;
 `;
 const OrderHistoryWrap = styled.ul`
 	display: flex;
@@ -110,7 +110,9 @@ const GoShoppingButton = styled.button`
 	background-color: #221814;
 `;
 const OrderHistoryList = styled.li`
+	position: relative;
 	margin-bottom: 2rem;
+	margin-top: 2rem;
 	width: 69.7rem;
 	height: 17.6rem;
 	background-color: #fff;
@@ -121,88 +123,67 @@ const OrderHistoryList = styled.li`
 		margin: 0;
 	}
 `;
+const OrderTopDate = styled.p`
+	position: absolute;
+	font-size: 1.6rem;
+	font-family: 'ro-b';
+	letter-spacing: -0.64px;
+	color: #221814;
+	top: -2.5rem;
+`;
+const OrderId = styled.span`
+	font-family: 'ro-r';
+	color: #8e8e8e;
+	margin-left: 0.2rem;
+`;
 const ListImg = styled.img`
 	width: 27.5%;
 	height: 100%;
 `;
 const ListContents = styled.div`
-	width: 47.5%;
+	width: 100%;
 	height: 100%;
 	padding: 2rem;
 `;
-const OrderTop = styled.div`
-	height: 2.4rem;
+const ProductNameBox = styled.div`
+	width: 31rem;
 	display: flex;
-	align-items: center;
-	margin-bottom: 1.7rem;
-`;
-const OrderTopText = styled.p`
-	height: 2.4rem;
-	line-height: 2.4rem;
-	font-size: 1.6rem;
-	font-family: 'kr-b';
-	${(props) => props.date && `color:#221814;`}
-	${(props) => props.state && `color:#E50011; margin-left:2rem;`}
+	margin-bottom: 0.4rem;
+	align-items: flex-end;
 `;
 const ProductName = styled.p`
-	height: 4.2rem;
-	line-height: 2.1rem;
-	margin-bottom: 1.7rem;
-	font-size: 1.4rem;
-	font-family: 'kr-r';
+	max-width: 25rem;
+	font-size: 2rem;
+	font-family: 'kr-b';
 	color: #221814;
-	display: -webkit-box;
 	text-overflow: ellipsis;
 	overflow: hidden;
-	-ms-line-clamp: 2;
-	-moz-line-clamp: 2;
-	-webkit-line-clamp: 2;
-	-webkit-box-orient: vertical;
 `;
-const ProductOption = styled.p`
-	margin-bottom: 0.2rem;
-	height: 1.7rem;
-	line-height: 1.7rem;
-	font-size: 1.2rem;
+const ProductNameSpan = styled.span`
+	font-size: 1.8rem;
 	font-family: 'kr-r';
-	color: #221814;
+	margin-left: 0.2rem;
 `;
-const ProductCount = styled.p`
-	height: 1.7rem;
-	line-height: 1.7rem;
-	font-size: 1.2rem;
-	font-family: 'kr-r';
-	color: #221814;
-`;
-const ListButtons = styled.div`
-	width: 25%;
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-`;
-const ListButton = styled.button`
-	width: 13.5rem;
-	height: 4rem;
-	line-height: 4rem;
+const DeliveryAddr = styled.p`
 	font-size: 1.4rem;
 	font-family: 'kr-r';
 	letter-spacing: -0.56px;
-	margin-bottom: 0.6rem;
-	border-radius: 4px;
-	border: none;
-	background-color: unset;
-	${(props) =>
-		props.last
-			? `margin-bottom:0; color:#E50011; border:1px solid #E50011;`
-			: `color:#fff; background-color:#221814`}
+	color: #6b6462;
 `;
+const ProductTotalPrice = styled.p`
+	font-size: 1.6rem;
+	font-family: 'kr-b';
+	letter-spacing: -0.64px;
+	color: #221814;
+	margin-top: 5.9rem;
+`;
+
 const GrayBackground = styled.div`
 	width: 192rem;
 	height: 37.3rem;
 	background-color: #f2f2f2;
 	z-index: -1;
 	position: absolute;
-	bottom: 0;
+	/* bottom: 0; */
+	top: 13.5rem;
 `;
