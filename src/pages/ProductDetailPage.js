33d5a9118ea3f860443c19255f984df3905e0440
Uni <joseoyun12@gmail.com>
2022-02-25 15:47:15 +0900

    psps 상품상세페이지 들어오면 상단으로 스크롤

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -6,14 +6,25 @@ import ProductDetail from '../components/ProductDetailPage/ProductDetail';
 import ProductInfoTable from '../components/ProductDetailPage/ProductInfoTable';
 import Induce from '../components/Induce';
 import Footer from '../components/Footer';
+import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 const ProductDetailPage = () => {
+	const { pathname } = useLocation();
+	const user = useSelector((state) => state.user);
 	const { product_id } = useParams();
 	const selectRef = useRef();
 	const infoRef = useRef();
 	const detailRef = useRef();
 	const [detail, setDetail] = useState({});
 	const [optionList, setOptionList] = useState([]);
+
+	function ScrollToTop() {
+		useEffect(() => {
+			window.scrollTo(0, 0);
+		}, [pathname]);
+		return null;
+	}
 
 	useEffect(() => {
 		if (product_id) {
@@ -32,7 +43,13 @@ const ProductDetailPage = () => {
 	}, [product_id]);
 	return (
 		<div id="container">
-			<OrderBox detail={detail} optionList={optionList} selectRef={selectRef} />
+			<ScrollToTop />
+			<OrderBox
+				detail={detail}
+				optionList={optionList}
+				selectRef={selectRef}
+				user={user}
+			/>
 			<ProductDetail
 				detail={detail}
 				detailRef={detailRef}
