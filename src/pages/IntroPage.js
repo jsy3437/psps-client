4eeaa82df84d4d129e0ba1a94d06c8b7a64ad4bb
Uni <joseoyun12@gmail.com>
2022-04-16 21:33:36 +0900

    소개글 한정된 크기에서만 움직일 수 있게 감싸주고, 이미지 사이즈 조정후 수정

--- src/pages/IntroPage.js
+++ src/pages/IntroPage.js
@@ -14,36 +14,40 @@ const IntroPage = () => {
 		<div id="container">
 			<Container>
 				<BackgroundImg alt="intro banner" src={bnn_top} />
-				<IntroTextBox top>
-					<IntroTextTitle>{intro[0].title}</IntroTextTitle>
-					<IntroText>{intro[0].text}</IntroText>
-					<IntroTextPoint>{intro[0].point}</IntroTextPoint>
-				</IntroTextBox>
-				<IntroTextBox middle={intro[1].src}>
-					<IntroTextTitle>{intro[1].title}</IntroTextTitle>
-					<IntroText>{intro[1].text}</IntroText>
-					<IntroTextPoint>{intro[1].point}</IntroTextPoint>
-				</IntroTextBox>
-				<IntroWorryBox top>
-					<WorryImg alt="worry image" src={intro[2].src} />
-					<IntroTextPoint>{intro[2].point}</IntroTextPoint>
-					<IntroText>{intro[2].text}</IntroText>
-				</IntroWorryBox>
-				<IntroWorryBox middle>
-					<WorryImg alt="worry image" src={intro[3].src} />
-					<IntroTextPoint>{intro[3].point}</IntroTextPoint>
-					<IntroText>{intro[3].text}</IntroText>
-				</IntroWorryBox>
-				<IntroWorryBox bottom>
-					<WorryImg alt="worry image" src={intro[4].src} />
-					<IntroTextPoint>{intro[4].point}</IntroTextPoint>
-					<IntroText>{intro[4].text}</IntroText>
-				</IntroWorryBox>
-				<IntroTextBox bottom>
-					<IntroTextTitle>{intro[5].title}</IntroTextTitle>
-					<IntroText>{intro[5].text}</IntroText>
-					<IntroTextPoint>{intro[5].point}</IntroTextPoint>
-				</IntroTextBox>
+				<InsideWarp>
+					<IntroTextBox top>
+						<IntroTextTitle>{intro[0].title}</IntroTextTitle>
+						<IntroText>{intro[0].text}</IntroText>
+						<IntroTextPoint>{intro[0].point}</IntroTextPoint>
+					</IntroTextBox>
+					<IntroTextBox middle={intro[1].src}>
+						<IntroTextTitle>{intro[1].title}</IntroTextTitle>
+						<IntroText>{intro[1].text}</IntroText>
+						<IntroTextPoint>{intro[1].point}</IntroTextPoint>
+					</IntroTextBox>
+					<WorryWrap>
+						<IntroWorryBox top>
+							<WorryImg alt="worry image" src={intro[2].src} />
+							<IntroTextPoint>{intro[2].point}</IntroTextPoint>
+							<IntroText>{intro[2].text}</IntroText>
+						</IntroWorryBox>
+						<IntroWorryBox middle>
+							<WorryImg alt="worry image" src={intro[3].src} />
+							<IntroTextPoint>{intro[3].point}</IntroTextPoint>
+							<IntroText>{intro[3].text}</IntroText>
+						</IntroWorryBox>
+						<IntroWorryBox bottom>
+							<WorryImg alt="worry image" src={intro[4].src} />
+							<IntroTextPoint>{intro[4].point}</IntroTextPoint>
+							<IntroText>{intro[4].text}</IntroText>
+						</IntroWorryBox>
+					</WorryWrap>
+					<IntroTextBox bottom>
+						<IntroTextTitle>{intro[5].title}</IntroTextTitle>
+						<IntroText>{intro[5].text}</IntroText>
+						<IntroTextPoint>{intro[5].point}</IntroTextPoint>
+					</IntroTextBox>
+				</InsideWarp>
 				<BackgroundImg alt="intro banner" src={bnn_bottom} />
 			</Container>
 			<Induce />
@@ -55,6 +59,7 @@ export default IntroPage;
 
 const Container = styled.div`
 	width: 192rem;
+	width: 100%;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
@@ -68,16 +73,25 @@ const Container = styled.div`
 const BackgroundImg = styled.img`
 	width: 100%;
 	height: 66.2rem;
+	height: 100%;
+	object-fit: contain;
+`;
+const InsideWarp = styled.div`
+	max-width: 137.3rem;
+	width: 100%;
+	z-index: 99;
+	padding: 0 2rem;
 `;
 const IntroTextBox = styled.div`
 	width: 120rem;
+	width: 100%;
 	margin: auto;
 	white-space: pre-wrap;
 	${(props) => props.top && `text-align:start; position:relative; top:-17rem;`}
 	${(props) =>
 		props.middle &&
-		`width:83.7rem; height:83.7rem; text-align:end; background-image:url(${props.middle}); background-size:100% 100%; padding:16.8rem 17rem 17rem 20rem; margin:-25em 18.7rem 0 auto; `}
-		${(props) => props.bottom && `margin-bottom:-15rem; z-index:99`}
+		`width:83.7rem; height:83.7rem; text-align:end; background-image:url(${props.middle}); background-size:100% 100%; padding:16rem 17rem 17rem 20rem; margin:-25em  0 0 auto; `}
+		${(props) => props.bottom && `margin-bottom:-15rem;`}
 `;
 const IntroTextTitle = styled.h2`
 	font-size: 8rem;
@@ -101,17 +115,20 @@ const IntroTextPoint = styled.p`
 	letter-spacing: -0.208rem;
 	color: #221814;
 `;
+const WorryWrap = styled.div`
+	width: 100%;
+	padding-right: 17rem;
+`;
 const IntroWorryBox = styled.div`
 	width: 125rem;
+	width: 100%;
 	margin: -23rem auto 0;
-	${(props) => props.top && `text-align:start; z-index:99`}
-	${(props) =>
-		props.middle && `text-align:center; margin: -24rem auto 0;z-index:98;`}
-	${(props) =>
-		props.bottom && `text-align:end; margin: -22rem auto 0; z-index:97;`}
+	${(props) => props.top && `text-align:start;`}
+	${(props) => props.middle && `text-align:center; margin: -4rem auto 0;`}
+	${(props) => props.bottom && `text-align:end; margin: -2rem auto 0;`}
 `;
 const WorryImg = styled.img`
 	width: 38rem;
-	height: 50.1rem;
-	margin-bottom: -9rem;
+	/* height: 50.1rem; */
+	margin-bottom: -5rem;
 `;
